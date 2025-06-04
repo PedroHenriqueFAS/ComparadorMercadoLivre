357199ccf1018dbaae4df1a871a25ccf1ea87919
Pedro <pedrohenriquefa97@gmail.com>
2025-06-03 21:28:33 -0300

    Refactor main.py to improve code organization and readability

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import os
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 path = os.getcwd() + '/chromedriver.exe'
 
@@ -10,3 +12,18 @@ options = webdriver.ChromeOptions()
 service = Service(executable_path = path)
 
 driver = webdriver.Chrome(options=options, service=service)
+
+LINK = "https://www.mercadolivre.com.br/" 
+
+driver.get(LINK)
+time.sleep(1)
+barra_pesquisa = driver.find_element(By.ID, "cb1-edit")
+
+produto_input = input('Digite o produto que deseja pesquisar: ')
+
+barra_pesquisa.send_keys(produto_input)
+time.sleep(1)
+barra_pesquisa.send_keys(Keys.ENTER)
+time.sleep(2)
+
+input('Pressione Enter para finalizar')
